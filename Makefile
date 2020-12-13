7dac3362fb6fa2a1cc9bc14e1cd3b93485d2bc63
Exr0n <mail@exr0n.com>
2020-12-13 03:07:09 -0800

    basic mockup

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 debug: main.cpp
-	g++ -std=c++20 $(sdl2-config --cflags --libs) -lX11 main.cpp -o auto 
+	g++ -std=c++20 $(sdl2-config --cflags --libs) -lSDL2_gfx -lX11 main.cpp -o auto 
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,42 @@
 #include <SDL2/SDL.h>
 #include <X11/Xlib.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <vector>
+#include <algorithm>
 
-struct Object
-{
-}
+const int WIDTH = 3000;
+const int HEIGHT = 1920;
 
-struct Body: Object
-{
-}
+const int CENTER_X = 800;
+const int CENTER_Y = 600;
+const int RADIUS = 2;
+const int LAYERS = 9;
+const unsigned long SUNCOLOR = 0xee339900;
 
-struct Collider: Object
+int rng(int l, int r)
+{ return rand() % (r-l+1) + l; }
+
+struct Triangle
 {
-}
+    float x, y, w, h, s;
+    int cr, cg, cb;
+    void render(SDL_Renderer* renderer)
+    {
+        filledTrigonRGBA(renderer, x, y, x+w/2, y-h, x+w, y, cr, cg, cb, 0xff);
+        x = (x - s);
+        if (x+w < 0) x += WIDTH+w;   // TODO sketchy mod
+    }
+};
 
 int main()
 {
+    std::vector<Triangle> layer;
+    for (int i=0; i<40; ++i)
+        layer.emplace_back(300*i, 1000, 700+rng(-100, 100), 400+rng(-200, 200), 0.1, 0x44, 0x33, 0x33);
+    for (int i=0; i<90; ++i)
+        layer.emplace_back(50*i, 1000, 70+rng(-20, 20), 120+rng(-20, 20), 0.4, 0x05, 0x55, 0x05);
+    //for (int i=1; i<=10; ++i)
+    //    layer.emplace_back(10, 1000, 50, 90);
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -28,24 +49,20 @@ int main()
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindowFrom((void*)x11w);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1080, 1920);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
 
-    SDL_Rect r;
-    r.w = 200;
-    r.h = 150;
-    r.x = 100;
-    r.y = 100;
     for (SDL_Event event = {}; event.type != SDL_QUIT; SDL_PollEvent(&event))
     {
-        r.x = (r.x + 1) % 1080;
         // clear, I guess?
         SDL_SetRenderTarget(renderer, texture);
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
-        // draw the rect
-        SDL_RenderDrawRect(renderer,&r);
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
-        SDL_RenderFillRect(renderer, &r);
+        // draw triangles
+        for (auto &tri : layer)
+            tri.render(renderer);
+        // draw circles
+        for (int i=0; i<LAYERS; ++i)
+            filledCircleRGBA(renderer, CENTER_X, CENTER_Y, RADIUS<<i, 0xff, 0xee, 0x20, 0x40*i/LAYERS);
         // draw to the screen?
         SDL_SetRenderTarget(renderer, NULL);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
