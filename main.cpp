b72456e99fc2e497dcd74992ca5d5889cc246d2c
Exr0n <mail@exr0n.com>
2020-12-19 21:00:25 -0800

    working layers and colors

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <X11/Xlib.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <vector>
+#include <chrono>
+#include <thread>
 #include <array>
 #include <memory>
+#include <iostream>
 #include <algorithm>
 
 // TODO: auto-get height https://stackoverflow.com/questions/33393528/how-to-get-screen-size-in-sdl
@@ -19,12 +22,12 @@ typedef std::array<unsigned char, 4> color_t;
 
 const size_t NUM_LAYERS = 6;
 const color_t layer_colors[NUM_LAYERS] = {
-    { 33,  88,  197 },
-    { 97,  155, 255 },
-    { 109, 178, 255 },
-    { 128, 187, 255 },
-    { 145, 213, 255 },
-    { 173, 219, 253 },
+    { 33,  88,  197, 0xff },
+    { 97,  155, 255, 0xff },
+    { 109, 178, 255, 0xff },
+    { 128, 187, 255, 0xff },
+    { 145, 213, 255, 0xff },
+    { 173, 219, 253, 0xff },
 };
 
 int rng(int l, int r)
@@ -61,6 +64,7 @@ public:
     color_t color;
     std::vector<std::unique_ptr<Renderable> > assets;
     // constructors
+    Layer(): speed(0), ypos(0), color({0xff, 0, 0, 0}), assets() {};
     Layer(float speed, float ypos, color_t color): speed(speed), ypos(ypos), color(color) {}
     // methods
     void render(SDL_Renderer* renderer)
@@ -75,11 +79,12 @@ int main()
     //std::array<Layer, NUM_LAYERS> layers = {{ {0.1, 1000, {0x44, 0x33, 0x33, 0xff} }, { 0.4, 1000, {0x05, 0x55, 0x05, 0xff} } }};
     std::array<Layer, NUM_LAYERS> layers;
     for (int i=0; i<NUM_LAYERS; ++i)
-        layers[i] = { 0.1*(i+1), 1400, layer_colors[i] };
+        layers[i] = { 0.1*(i+1), 800, layer_colors[i] };
     for (int i=0; i<40; ++i)
-        layers[0].assets.emplace_back( new Triangle(300*i, 1000, 700+rng(-100, 100), 400+rng(-200, 200)) );
+        layers[0].assets.emplace_back(new Triangle(300*i, 1200, 700+rng(-100, 100), 400+rng(-200, 200)));
+        //layers[0].assets.push_back(std::make_unique<Triangle>(100*i, 1200, 700+rng(-100, 100), 400+rng(-200, 200)) );
     for (int i=0; i<90; ++i)
-        layers[1].assets.emplace_back( new Triangle(50*i, 1000, 70+rng(-20, 20), 120+rng(-20, 20)) );
+        layers[1].assets.emplace_back( new Triangle(50*i, 1200, 70+rng(-20, 20), 120+rng(-20, 20)) );
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -111,6 +116,8 @@ int main()
         SDL_SetRenderTarget(renderer, NULL);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(100ms);
     }
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
