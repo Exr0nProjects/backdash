e37b6252be54b6af415a97df7720855ab331d64a
Exr0n <mail@exr0n.com>
2020-12-19 19:33:01 -0800

    vector of base class cant hold derived big brain

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
+main: debug
+	./auto
 
 debug: main.cpp
-	g++ -std=c++20 $(sdl2-config --cflags --libs) -lSDL2_gfx -lX11 main.cpp -o auto 
+	g++ -std=c++20 $(shell sdl2-config --cflags --libs) -lSDL2_gfx -lX11 main.cpp -o auto
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <X11/Xlib.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <vector>
+#include <array>
 #include <algorithm>
 
 // TODO: auto-get height https://stackoverflow.com/questions/33393528/how-to-get-screen-size-in-sdl
@@ -13,7 +14,7 @@ const int CENTER_Y = 600;
 const int RADIUS = 2;
 const int LAYERS = 9;
 const unsigned long SUNCOLOR = 0xee339900;
-typedef unsigned char color_t;
+typedef std::array<unsigned char, 4> color_t;
 
 int rng(int l, int r)
 { return rand() % (r-l+1) + l; }
@@ -22,31 +23,51 @@ class Renderable
 {
 public:
     float x, y, w, h;
+    // constructors
     Renderable(float x, float y, float w, float h): x(x), y(y), w(w), h(h) {}
-    virtual void render(SDL_Renderer* renderer, float speed, color_t r, color_t g, color_t b) = 0;
+    // methods
+    virtual void render(SDL_Renderer* renderer, float speed, color_t color) = 0;
 };
 
 class Triangle : public Renderable
 {
 public:
+    // constructors
     Triangle(float x, float y, float w, float h): Renderable(x, y, w, h) {}
-    void render(SDL_Renderer* renderer, float speed, color_t r, color_t g, color_t b)
+    // methods
+    void render(SDL_Renderer* renderer, float speed, color_t color)
     {
-        filledTrigonRGBA(renderer, x, y, x+w/2, y-h, x+w, y, r, g, b, 0xff);
+        filledTrigonRGBA(renderer, x, y, x+w/2, y-h, x+w, y, color[0], color[1], color[2], color[3]);
         x = (x - speed);
         if (x+w < 0) x += WIDTH+w;   // TODO sketchy mod
     }
 };
 
+class Layer
+{
+public:
+    float speed, ypos;
+    color_t color;
+    std::vector<Renderable> assets;
+    // constructors
+    Layer(float speed, float ypos, color_t color): speed(speed), ypos(ypos), color(color) {}
+    // methods
+    void render(SDL_Renderer* renderer)
+    {
+        for (auto &asset : assets)
+            asset.render(renderer, speed, color);
+    }
+};
+
 int main()
 {
-    std::vector<Triangle> layer;
+    //std::vector<Triangle> layer;
+    //std::array<Layer, 2> layers = { {0.1, 1000, {0x44, 0x33, 0x33, 0xff} }, { 0.4, 1000, {0x05, 0x55, 0x05, 0xff} } };
+    std::array<Layer, 2> layers = { {0.1, 1000, {0x44, 0x33, 0x33, 0xff} } };
     for (int i=0; i<40; ++i)
-        //layer.emplace_back(300*i, 1000, 700+rng(-100, 100), 400+rng(-200, 200), 0x44, 0x33, 0x33);
-        layer.emplace_back((float)300*i, (float)1000, (float)700+rng(-100, 100), (float)400+rng(-200, 200));
+        layers[0].assets.emplace_back((float)300*i, (float)1000, (float)700+rng(-100, 100), (float)400+rng(-200, 200));
     for (int i=0; i<90; ++i)
-        //layer.emplace_back(50*i, 1000, 70+rng(-20, 20), 120+rng(-20, 20), 0x05, 0x55, 0x05);
-        layer.emplace_back((float)50*i, (float)1000, (float)70+rng(-20, 20), (float)120+rng(-20, 20));
+        layers[1].assets.emplace_back((float)50*i, (float)1000, (float)70+rng(-20, 20), (float)120+rng(-20, 20));
     //for (int i=1; i<=10; ++i)
     //    layer.emplace_back(10, 1000, 50, 90);
 
@@ -70,9 +91,9 @@ int main()
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
         // draw the scene
-
-        for (auto &tri : layer)
-            tri.render(renderer, 0.1, 0x32, 0x6c, 0xcc);
+        for (auto &layer : layers) layer.render(renderer);
+        //for (auto &tri : layer)
+        //    tri.render(renderer, 0.1, 0x32, 0x6c, 0xcc);
         // draw sky
         for (int i=0; i<LAYERS; ++i)
             filledCircleRGBA(renderer, CENTER_X, CENTER_Y, RADIUS<<i, 0xff, 0xee, 0x20, 0x40*i/LAYERS);
