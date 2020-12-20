52fb0a3968365c7fe3ee064266d1a4c116d1b5ac
Exr0n <mail@exr0n.com>
2020-12-19 20:01:47 -0800

    NW: array initialization bork?

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <vector>
 #include <array>
+#include <memory>
 #include <algorithm>
 
 // TODO: auto-get height https://stackoverflow.com/questions/33393528/how-to-get-screen-size-in-sdl
@@ -11,10 +12,20 @@ const int HEIGHT = 1920;
 
 const int CENTER_X = 800;
 const int CENTER_Y = 600;
-const int RADIUS = 2;
-const int LAYERS = 9;
+const int RADIUS = 70;
+const int LAYERS = 7;
 const unsigned long SUNCOLOR = 0xee339900;
 typedef std::array<unsigned char, 4> color_t;
+
+const size_t NUM_LAYERS = 6;
+const color_t layer_colors[NUM_LAYERS] = {
+    { 33,  88,  197 },
+    { 97,  155, 255 },
+    { 109, 178, 255 },
+    { 128, 187, 255 },
+    { 145, 213, 255 },
+    { 173, 219, 253 },
+};
 
 int rng(int l, int r)
 { return rand() % (r-l+1) + l; }
@@ -48,28 +59,27 @@ class Layer
 public:
     float speed, ypos;
     color_t color;
-    std::vector<Renderable> assets;
+    std::vector<std::unique_ptr<Renderable> > assets;
     // constructors
     Layer(float speed, float ypos, color_t color): speed(speed), ypos(ypos), color(color) {}
     // methods
     void render(SDL_Renderer* renderer)
     {
         for (auto &asset : assets)
-            asset.render(renderer, speed, color);
+            asset->render(renderer, speed, color);
     }
 };
 
 int main()
 {
-    //std::vector<Triangle> layer;
-    //std::array<Layer, 2> layers = { {0.1, 1000, {0x44, 0x33, 0x33, 0xff} }, { 0.4, 1000, {0x05, 0x55, 0x05, 0xff} } };
-    std::array<Layer, 2> layers = { {0.1, 1000, {0x44, 0x33, 0x33, 0xff} } };
+    //std::array<Layer, NUM_LAYERS> layers = {{ {0.1, 1000, {0x44, 0x33, 0x33, 0xff} }, { 0.4, 1000, {0x05, 0x55, 0x05, 0xff} } }};
+    std::array<Layer, NUM_LAYERS> layers;
+    for (int i=0; i<NUM_LAYERS; ++i)
+        layers[i] = { 0.1*(i+1), 1400, layer_colors[i] };
     for (int i=0; i<40; ++i)
-        layers[0].assets.emplace_back((float)300*i, (float)1000, (float)700+rng(-100, 100), (float)400+rng(-200, 200));
+        layers[0].assets.emplace_back( new Triangle(300*i, 1000, 700+rng(-100, 100), 400+rng(-200, 200)) );
     for (int i=0; i<90; ++i)
-        layers[1].assets.emplace_back((float)50*i, (float)1000, (float)70+rng(-20, 20), (float)120+rng(-20, 20));
-    //for (int i=1; i<=10; ++i)
-    //    layer.emplace_back(10, 1000, 50, 90);
+        layers[1].assets.emplace_back( new Triangle(50*i, 1000, 70+rng(-20, 20), 120+rng(-20, 20)) );
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -96,7 +106,7 @@ int main()
         //    tri.render(renderer, 0.1, 0x32, 0x6c, 0xcc);
         // draw sky
         for (int i=0; i<LAYERS; ++i)
-            filledCircleRGBA(renderer, CENTER_X, CENTER_Y, RADIUS<<i, 0xff, 0xee, 0x20, 0x40*i/LAYERS);
+            filledCircleRGBA(renderer, CENTER_X, CENTER_Y, RADIUS*i, 0xff, 0xee, 0x20, 0x40*i/LAYERS);
         // draw to the screen?
         SDL_SetRenderTarget(renderer, NULL);
         SDL_RenderCopy(renderer, texture, NULL, NULL);
